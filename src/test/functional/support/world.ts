eb6d744dfe9069975f1180b69356a348587e6f5d
velimd <duzgunce.vd@gmail.com>
2020-09-08 13:22:57 +0100

    including headless browser for pipeline

--- src/test/functional/support/world.ts
+++ src/test/functional/support/world.ts
@@ -1,9 +1,11 @@
 import seleniumWebdriver from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
 import { setWorldConstructor } from 'cucumber';
 
 function CustomWorld() {
   this.driver = new seleniumWebdriver.Builder()
     .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().headless())
     .build();
 }
 
