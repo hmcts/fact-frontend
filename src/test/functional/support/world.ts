725e88c7447deeb4e52d18091a93c8f9a4f9eb8a
velimd <duzgunce.vd@gmail.com>
2020-09-08 18:10:16 +0100

    set path to chromedriver

--- src/test/functional/support/world.ts
+++ src/test/functional/support/world.ts
@@ -1,6 +1,10 @@
-import { Builder } from 'selenium-webdriver';
+import { Builder, Capabilities } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
+import { path } from 'chromedriver';
 import { setWorldConstructor } from 'cucumber';
+
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
 
 const options = new chrome.Options();
 options.addArguments('-headless');
@@ -8,6 +12,7 @@ options.addArguments('-headless');
 function CustomWorld() {
   this.driver = new Builder()
     .forBrowser('chrome')
+    .withCapabilities(Capabilities.chrome())
     .setChromeOptions(options)
     .build();
 }
