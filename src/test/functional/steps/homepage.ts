a1cbc31bfd6366c44641173658c168dcc914fba0
velimd <duzgunce.vd@gmail.com>
2020-09-09 17:17:21 +0100

    removing selenium and using puppeteer

--- src/test/functional/steps/homepage.ts
+++ src/test/functional/steps/homepage.ts
@@ -2,12 +2,14 @@ import { Given, Then } from 'cucumber';
 import { expect } from 'chai';
 
 import { config } from '../../config';
+const scope = require('../support/scope');
 
-Given('I am on FACT homepage', function() {
-  return this.driver.get(config.TEST_URL);
+Given('I am on FACT homepage', async function() {
+  scope.page = await scope.browser.newPage();
+  await scope.page.goto(config.TEST_URL);
 });
 
 Then('I expect the page header to be {string}', async function(title: string) {
-  const pageTitle = await this.driver.getTitle();
+  const pageTitle = await scope.page.title();
   expect(pageTitle).equal(title);
 });
--- src/test/functional/support/hooks.ts
+++ src/test/functional/support/hooks.ts
@@ -1,5 +1,19 @@
-import { After } from 'cucumber';
+import { BeforeAll, AfterAll, After } from 'cucumber';
+const scope = require('./scope');
+import puppeteer from 'puppeteer';
 
-After(function() {
-  return this.driver.quit();
+BeforeAll(async () => {
+  scope.browser = await puppeteer.launch();
+});
+
+After(async () => {
+  if (scope.page && scope.page.currentPage) {
+    scope.page.currentPage.close();
+  }
+});
+
+AfterAll(async () => {
+  if (scope.browser) {
+    await scope.browser.close();
+  }
 });
