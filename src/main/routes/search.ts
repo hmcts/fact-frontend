b5e956a2af93e584e5f3b2dde25a9e5b6ebfd964
velimd <duzgunce.vd@gmail.com>
2020-09-15 16:58:27 +0100

    clean up naming to match prototype urls

--- src/main/routes/search.ts
+++ src/main/routes/search.ts
@@ -2,20 +2,20 @@ import { Application, Request, Response } from 'express';
 
 export default function(app: Application): void {
 
-  app.get('/name', (req: any, res: any) => {
-    res.render('search/name', req.i18n.getDataByLanguage(req.lng).search.name);
+  app.get('/search-option', (req: any, res: any) => {
+    res.render('search/option', req.i18n.getDataByLanguage(req.lng).search.option);
   });
 
-  app.post('/name', (req: Request, res: Response) => {
+  app.post('/search-option', (req: Request, res: Response) => {
     const { knowName } = req.body;
     if (knowName === 'yes') {
-      return res.redirect('/search');
+      return res.redirect('/location-search');
     }
     // TODO story if the user doesnt know the name
     return res.redirect('/');
   });
 
-  app.get('/search', (req: any, res: any) => {
-    res.render('search/search', req.i18n.getDataByLanguage(req.lng).search.search);
+  app.get('/location-search', (req: any, res: any) => {
+    res.render('search/location', req.i18n.getDataByLanguage(req.lng).search.location);
   });
 }
