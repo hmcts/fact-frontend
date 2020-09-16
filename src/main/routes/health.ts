ada6038461bb73cddd37e41297f247793b1a5dc3
velimd <duzgunce.vd@gmail.com>
2020-09-16 11:36:42 +0100

    changing config import to nodejs style

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -1,5 +1,5 @@
 import { Application, Request } from 'express';
-import config from 'config';
+const config = require('config');
 
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 
@@ -19,7 +19,7 @@ export default function(app: Application): void {
 
   const healthCheckConfig = {
     checks: {
-      'fact-api': healthcheck.web(`${config.get('services.api.url')}/health`, healthOptions),
+      'fact-api': healthcheck.web(`${config.services.api.url}/health`, healthOptions),
     },
   };
 
