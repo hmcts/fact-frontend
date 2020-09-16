41469798d014aaac32d86c7fae1c881731c7b5f6
velimd <duzgunce.vd@gmail.com>
2020-09-16 15:14:28 +0100

    including controller, unit test and updating routes test

--- src/main/routes/search.ts
+++ src/main/routes/search.ts
@@ -1,21 +1,12 @@
-import { Application, Request, Response } from 'express';
+import { Application } from 'express';
+
+import { getSearchOption, postSearchOption, getLocationSearch } from '../controllers/search';
 
 export default function(app: Application): void {
 
-  app.get('/search-option', (req: any, res: any) => {
-    res.render('search/option', req.i18n.getDataByLanguage(req.lng).search.option);
-  });
+  app.get('/search-option', getSearchOption);
 
-  app.post('/search-option', (req: Request, res: Response) => {
-    const { knowName } = req.body;
-    if (knowName === 'yes') {
-      return res.redirect('/location-search');
-    }
-    // TODO story if the user doesnt know the name
-    return res.redirect('/');
-  });
+  app.post('/search-option', postSearchOption);
 
-  app.get('/location-search', (req: any, res: any) => {
-    res.render('search/location', req.i18n.getDataByLanguage(req.lng).search.location);
-  });
+  app.get('/location-search', getLocationSearch);
 }
--- src/test/routes/search.ts
+++ src/test/routes/search.ts
@@ -17,6 +17,16 @@ describe('search', () => {
         .send({ knowName: 'yes' })
         .expect((res) => expect(res.status).to.equal(302));
     });
+
+    // TODO this will change in the future when we have other stories implemented
+    test('should redirect home page', async () => {
+      await request(app)
+        .post('/search-option')
+        .send({ knowName: 'no' })
+        .expect((res) => {
+          expect(res.status).to.equal(302);
+        });
+    });
   });
 
   describe('search option', () => {
