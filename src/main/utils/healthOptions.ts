dadf1f238a5dcedb1eab03ba11a5e0b733c20f37
velimd <duzgunce.vd@gmail.com>
2020-09-17 17:01:47 +0100

    cleaning up health unit tests

--- src/main/utils/healthOptions.ts
+++ src/main/utils/healthOptions.ts
@@ -7,7 +7,7 @@ export const healthOptions = (): {} => {
   return {
     callback: (err: Error, res: Request): Promise<void> => {
       if (err) {
-        console.log('Health check failed on fact-api:');
+        console.log('Health check failed!');
       }
       return res.body.status == 'good' ? healthcheck.up() : healthcheck.down();
     },
