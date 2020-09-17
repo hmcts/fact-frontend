f67cdd6c1060b10a9074eb168d19f8892f0f2eef
velimd <duzgunce.vd@gmail.com>
2020-09-17 16:25:37 +0100

    including health page routes test

--- src/test/routes/health.ts
+++ src/test/routes/health.ts
@@ -7,7 +7,7 @@ describe('Health page', () => {
   describe('on GET', () => {
     test('should return health page', async () => {
       await request(app)
-        .get('/health')
+        .get('/health/liveness')
         .expect((res) => {
           expect(res.status).to.equal(200);
           expect(res.body).to.have.property('status').and.equal('UP');
