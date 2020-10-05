10347e27df42a13b66a4c0e5248f23947237cd3d
velimd <duzgunce.vd@gmail.com>
2020-10-05 15:41:14 +0100

    fix stub

--- api/fact-api-stubs.ts
+++ api/fact-api-stubs.ts
@@ -9,13 +9,13 @@ const port = 8080;
 app.use(bodyParser.json());
 
 app.get('/courts', (req: Request, res: Response) => {
-  const query: string = (req.query.search as string).toUpperCase();
+  const query: string = (req.query.q as string).toUpperCase();
   const courts = [...data];
   const filteredDocs = courts.filter(court => {
-    if (court['postcode'].toUpperCase().split(' ').indexOf(query) >= 0 ||
-        court['address'].toUpperCase().split(' ').indexOf(query) >= 0 ||
-        court['townName'].toUpperCase().split(' ').indexOf(query) >= 0 ||
-        court['name'].toUpperCase().split(' ').indexOf(query) >= 0) {
+    if (court['postcode'].toUpperCase().indexOf(query) >= 0 ||
+        court['address'].toUpperCase().indexOf(query) >= 0 ||
+        court['townName'].toUpperCase().indexOf(query) >= 0 ||
+        court['name'].toUpperCase().indexOf(query) >= 0) {
       return true;
     }
   });
