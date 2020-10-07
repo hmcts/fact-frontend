4d44d814851ecca2137dddd676cfc416cd2e6e82
SarahLittlejohn <sarahjanelittlejohn@gmail.com>
2020-10-07 13:43:29 +0100

    coverage: clean up test

--- src/test/unit/test/utils/healthOptions.ts
+++ src/test/unit/test/utils/healthOptions.ts
@@ -1,7 +1,7 @@
 import { healthOptions } from '../../../../main/utils/healthOptions';
 import config from 'config';
 
-const mockResponseGood = () => {
+const mockResponse = () => {
   const res: any = {
     body: {
       status: 'good',
@@ -10,18 +10,9 @@ const mockResponseGood = () => {
   return res;
 };
 
-const mockResponseNotGood = () => {
-  const res: any = {
-    body: {
-      status: 'not good',
-    },
-  };
-  return res;
-};
-
 describe('healthOptions', () => {
   test('Should return health options if status is good', async () => {
-    const res: any = mockResponseGood();
+    const res: any = mockResponse();
     const results: any = healthOptions();
     expect(results).toBeTruthy();
     expect(results.timeout).toBe(config.get('health.timeout'));
@@ -33,7 +24,7 @@ describe('healthOptions', () => {
   });
 
   test('Should return health options with error', async () => {
-    const res: any = mockResponseGood();
+    const res: any = mockResponse();
     const results: any = healthOptions();
     expect(results).toBeTruthy();
     expect(results.timeout).toBe(config.get('health.timeout'));
@@ -45,7 +36,8 @@ describe('healthOptions', () => {
   });
 
   test('Should return health check down if status is not good', async () => {
-    const res: any = mockResponseNotGood();
+    const res: any = mockResponse();
+    res.body.status = 'not good';
     const results: any = healthOptions();
     expect(results).toBeTruthy();
     expect(results.timeout).toBe(config.get('health.timeout'));
