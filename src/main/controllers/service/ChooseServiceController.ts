a43e254b8f9ccc41d770951521ce98c333350d1a
SarahLittlejohn <sarahjanelittlejohn@gmail.com>
2020-11-01 19:47:02 +0000

    FACT-59: Refactor controllers.

--- src/main/controllers/service/ChooseServiceController.ts
+++ src/main/controllers/service/ChooseServiceController.ts
@@ -1,7 +1,6 @@
 import { FactRequest } from '../../interfaces/FactRequest';
 import { Response } from 'express';
 import { hasProperty, isObjectEmpty } from '../../utils/validation';
-import { PageData } from '../../interfaces/PageData';
 import { ServicesData } from '../../interfaces/ServicesData';
 import { FactApi } from '../../utils/FactApi';
 import autobind from 'autobind-decorator';
@@ -14,48 +13,42 @@ export class ChooseServiceController {
     private readonly api: FactApi
   ) { }
 
-  public async get(req: FactRequest, res: Response) {
+  private async getServices(req: FactRequest, hasErrors: boolean) {
     const data: ServicesData = {
       ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['choose-service']),
       path: '/services',
       results: [],
+      errors: hasErrors,
     };
 
-    const areasOfLaw = await this.api.services();
-    if (!isObjectEmpty(areasOfLaw)) {
-      data.results = areasOfLaw;
+    const services = await this.api.services();
+    if (!isObjectEmpty(services)) {
+      data.results = services;
     }
+    return data;
+  }
+
+  public async get(req: FactRequest, res: Response) {
+    const data = await this.getServices(req, false);
     res.render('choose-service', data);
   }
 
   public async post(req: FactRequest, res: Response) {
     if (!hasProperty(req.body, 'chooseService')) {
-      const data: PageData = {
-        ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['choose-service']),
-        path: '/services',
-        errors: true,
-        results: [],
-      };
-
-      const chosenService = await this.api.services();
-      if (!isObjectEmpty(chosenService)) {
-        data.results = chosenService;
-      }
+      const data = await this.getServices(req, true);
       return res.render('choose-service', data);
     }
 
-    if (req.body.chooseService as string === 'serviceChosen1') {
-      return res.redirect('/services/Money/service-areas');
-    } else if (req.body.chooseService as string === 'serviceChosen2') {
-      return res.redirect('/services/Probate, divorce or ending civil partnerships/service-areas');
-    } else if (req.body.chooseService as string === 'serviceChosen3') {
-      return res.redirect('/services/Childcare and parenting/service-areas');
-    } else if (req.body.chooseService as string === 'serviceChosen4') {
-      return res.redirect('/services/Harm and abuse/service-areas');
-    } else if (req.body.chooseService as string === 'serviceChosen6') {
-      return res.redirect('/services/Crime/service-areas');
-    } else {
-      return res.redirect('/');
-    }
+    const serviceName: any = {
+      'serviceChosen1' : 'Money',
+      'serviceChosen2' : 'Probate, divorce or ending civil partnerships',
+      'serviceChosen3' : 'Childcare and parenting',
+      'serviceChosen4' : 'Harm and abuse',
+      'serviceChosen5' : 'Crime',
+    };
+
+    const serviceChosen = serviceName[req.body.chooseService];
+    return res.redirect('/services/' + serviceChosen + '/service-areas');
+
   }
 }
