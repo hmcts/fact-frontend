d618f4574a1eb21dfde19a91ee5cc8e1197afa60
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-11-06 18:20:04 +0000

    FACT-183: fix lint

--- src/main/controllers/HomeController.ts
+++ src/main/controllers/HomeController.ts
@@ -3,7 +3,7 @@ import { FactRequest } from '../interfaces/FactRequest';
 
 export class HomeController {
   public get(req: FactRequest, res: Response): void {
-    let data = req.i18n.getDataByLanguage(req.lng).home;
+    const data = req.i18n.getDataByLanguage(req.lng).home;
     Object.assign(data, req.lng);
     res.render('home', req.i18n.getDataByLanguage(req.lng).home);
   }
