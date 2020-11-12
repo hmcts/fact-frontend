934d51df73a14a9a6b19768ea08a5234370d7797
Shaun Francis <shaun.francis1@hmcts.net>
2020-11-12 21:24:52 +0000

    Fix pa11y timeout

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -55,7 +55,6 @@ function ensurePageCallWillSucceed(url: string): Promise<void> {
 function runPallyWith(url: string, actions: string[]): Pa11yResult {
   return pa11y(config.TEST_URL + url, {
     hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
-    timeout : 30000,
     actions: actions
   });
 }
