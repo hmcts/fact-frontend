253587eb9d86a6f333eae33b9f03ced4af160a6c
Sarah Littlejohn <sarahjanelittlejohn@gmail.com>
2020-11-15 09:49:35 +0000

    FACT-186: spacing

--- src/main/controllers/service/ChooseServiceAreaController.ts
+++ src/main/controllers/service/ChooseServiceAreaController.ts
@@ -54,10 +54,12 @@ export class ChooseServiceAreaController {
 
   public async post(req: FactRequest, res: Response) {
     const action = req.params.action as Action;
+
     if (!hasProperty(req.body, 'serviceArea')) {
       const serviceChosen = req.params.service as string;
       const serviceAreasPageData = req.i18n.getDataByLanguage(req.lng).service;
       const serviceData = await this.getServiceData(serviceChosen, action, serviceAreasPageData, true, req.lng);
+
       res.render('service', serviceData);
     } else if (req.body.serviceArea === 'not-listed') {
       res.redirect('/services/unknown-service');
