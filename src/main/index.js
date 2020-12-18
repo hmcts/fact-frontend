9e91f52754e160ea6ee83f27826ce0380d00c37d
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2020-12-18 10:57:30 +0000

    fix lint

--- src/main/index.js
+++ src/main/index.js
@@ -8,7 +8,7 @@ cookieManager.init({
   'user-preference-cookie-name': 'fact-cookie-preferences',
   'user-preference-configuration-form-id': 'cm_user_preference_form',
   'user-preference-saved-callback': function () {
-    const message = document.querySelector('#cookie-preference-success');
+    const message = document.querySelector('.cookie-preference-success');
     message.style.display = 'block';
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
