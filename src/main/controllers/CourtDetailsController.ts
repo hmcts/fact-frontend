2c48c3a0ea6bdcbe30d4bf97b7ef2af99f0c7fab
“Thomas <Thomas.Geraghty@justice.gov.uk>
2021-02-04 17:18:42 +0000

    FACT-319 Fix for req.get('host') being non-viable method of obtaining host address

--- src/main/controllers/CourtDetailsController.ts
+++ src/main/controllers/CourtDetailsController.ts
@@ -24,7 +24,7 @@ export class CourtDetailsController {
     const data: CourtDetailsData = {
       ...cloneDeep(req.i18n.getDataByLanguage(req.lng)['court-details']),
       path: '/courts/' + slug,
-      fullPath: req.get('host') + '/courts/' + slug,
+      fullPath: req.hostname + '/courts/' + slug,
       results: {},
     };
 
