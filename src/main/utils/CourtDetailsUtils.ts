2488d6387114f8c045d5e7af57ec8e06bd58257d
“Thomas <Thomas.Geraghty@justice.gov.uk>
2021-03-03 00:11:48 +0000

    FACT-357 - Added formatServiceAreas function for converting array of service areas into a grammatically correct string.

--- src/main/utils/CourtDetailsUtils.ts
+++ src/main/utils/CourtDetailsUtils.ts
@@ -1,3 +1,18 @@
+import { isArrayEmpty } from './validation';
+
 export const decideCatchmentArea = (regionalCentre: boolean, area: { area1: string; area2: string }): string => {
   return regionalCentre ? area.area1 : area.area2;
+};
+
+export const formatServiceAreas = (serviceAreas: string[]): string => {
+  if(isArrayEmpty(serviceAreas)) {
+    return '';
+  }
+
+  const lastElement: string = serviceAreas.pop();
+  let serviceAreaString: string = serviceAreas.join(', ');
+  serviceAreaString += serviceAreas.length >= 1 ? ' and ' : '';
+  serviceAreaString += lastElement;
+
+  return serviceAreaString.toLowerCase();
 };
