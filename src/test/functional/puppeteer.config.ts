a4afda33de27853eda83e7e03a05e8a619f011ad
Damon Green <damon.green@btinternet.com>
2021-03-31 11:35:48 +0100

    remove proxy

--- src/test/functional/puppeteer.config.ts
+++ src/test/functional/puppeteer.config.ts
@@ -8,8 +8,6 @@ export const puppeteerConfig = {
   defaultTimeout: 60 * 1000,
   args: [
     '--no-sandbox',
-    '--proxy-server=proxyout.reform.hmcts.net:8080',
-    '--proxy-bypass-list=*beta*LB.reform.hmcts.net',
     '--window-size=1440,1400',
   ],
 };
