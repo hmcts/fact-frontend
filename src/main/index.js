6749c78491295f4fe7aadea6fc29c277a80b942c
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-04-12 02:03:58 +0100

    FACT-389 - Cleaned up fact-cookie-manager config settings code and added consent for GA into callbacks

--- src/main/index.js
+++ src/main/index.js
@@ -2,49 +2,71 @@ import './assets/scss/main.scss';
 import {initAll} from 'govuk-frontend';
 
 const cookieManager = require('../../node_modules/@hmcts/fact-cookie-manager/cookie-manager.js');
+const cookieBanner = document.querySelector('#cookie_banner');
+const cookieBannerDecision = cookieBanner.querySelector('.govuk-cookie-banner__decision');
+const cookieBannerConfirmation = cookieBanner.querySelector('.govuk-cookie-banner__confirmation');
+
+function cookieBannerAccept() {
+  const confirmationMessage = cookieBannerConfirmation.querySelector('p');
+  confirmationMessage.innerHTML = 'You’ve accepted analytics cookies. ' + confirmationMessage.innerHTML;
+}
+
+function cookieBannerReject() {
+  const confirmationMessage = cookieBannerConfirmation.querySelector('p');
+  confirmationMessage.innerHTML = 'You’ve rejected analytics cookies. ' + confirmationMessage.innerHTML;
+}
+
+function cookieBannerSaved(cookieStatus) {
+  cookieBannerDecision.hidden = true;
+  cookieBannerConfirmation.hidden = false;
+  cookiePreferencesUpdated(cookieStatus);
+}
+
+function preferenceFormSaved(cookieStatus) {
+  const message = document.querySelector('.cookie-preference-success');
+  message.style.display = 'block';
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+  cookiePreferencesUpdated(cookieStatus);
+}
+
+function cookiePreferencesUpdated(cookieStatus) {
+  const dataLayer = window.dataLayer || [];
+  const gtag = window.gtag || function () { dataLayer.push(arguments); };
+  dataLayer.push({'event': 'cookies', 'preferences': cookieStatus});
+
+  if(cookieStatus.analytics === 'on') {
+    gtag('consent', 'update', { 'analytics_storage': 'granted' });
+  } else {
+    gtag('consent', 'update', { 'analytics_storage': 'denied' });
+  }
+}
+
 cookieManager.init({
   'cookie-banner-id': 'cookie_banner',
-  'cookie-banner-visibility-class': 'govuk-visually-hidden',
   'cookie-banner-visible-on-page-with-preference-form': false,
   'user-preference-cookie-name': 'fact-cookie-preferences',
   'user-preference-configuration-form-id': 'cm_user_preference_form',
-  'user-preference-saved-callback': function () {
-    const message = document.querySelector('.cookie-preference-success');
-    message.style.display = 'block';
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  },
-  'cookie-banner-reject-callback': function () {
-    const confirmation = document.querySelector('.govuk-cookie-banner__confirmation');
-    const confirmationMessage = confirmation.querySelector('p').innerHTML;
-    confirmation.querySelector('p').innerHTML = 'You’ve rejected analytics cookies. ' + confirmationMessage;
-    confirmation.removeAttribute('hidden');
-  },
-  'cookie-banner-accept-callback': function () {
-    const confirmation = document.querySelector('.govuk-cookie-banner__confirmation');
-    const confirmationMessage = confirmation.querySelector('p').innerHTML;
-    confirmation.querySelector('p').innerHTML = 'You’ve accepted analytics cookies. ' + confirmationMessage;
-    confirmation.removeAttribute('hidden');
-  },
-  'cookie-banner-saved-callback': function () {
-    const decision = document.querySelector('.govuk-cookie-banner__decision');
-    decision.setAttribute('hidden', true);
-  },
+  'user-preference-saved-callback': preferenceFormSaved,
+  'cookie-banner-reject-callback': cookieBannerReject,
+  'cookie-banner-accept-callback': cookieBannerAccept,
+  'cookie-banner-saved-callback': cookieBannerSaved,
   'set-checkboxes-in-preference-form': true,
   'cookie-banner-auto-hide': false,
   'cookie-manifest': [
     {
       'category-name': 'essential',
-      optional: false,
-      cookies: [
+      'optional': false,
+      'cookies': [
         'i18next',
         'fact-cookie-preferences'
       ]
     },
     {
       'category-name': 'analytics',
-      optional: true,
-      cookies: [
+      'optional': true,
+      'cookies': [
         '_ga',
         '_gid',
         '_gat_UA-37377084-3'
