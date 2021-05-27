44b71161b296e7ca8202bf64bf2ccbe420978cf5
kiankwa <kian.kwa@justice.gov.uk>
2021-05-27 17:20:07 +0100

    Update smoke test to check for the health endpoint status

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -4,10 +4,10 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 describe('Smoke Test', () => {
   describe('healthcheck', () => {
-    test('should return status 200', async () => {
-      const response = await superagent.get(config.TEST_URL);
-
+    test('Test that the frontend health endpoint shows all connected services as being up', async () => {
+      const response = await superagent.get(config.TEST_URL + '/health');
       expect(response.statusCode).toBe(200);
+      expect(response.body['status']).toBe('UP'); // If the fact-api or MapIt service is down, the overall status will be down
     });
   });
 });
