9851290237a46ec4ede7f3b009fa63b0665ae383
Ayisha-Sharjeel <ayisha.sharjeel@justice.gov.uk>
2021-05-28 11:17:42 +0100

    Functional test

--- src/test/functional/features/A-Z-court-search.feature
+++ src/test/functional/features/A-Z-court-search.feature
@@ -1,49 +1,31 @@
 Feature: A to Z Court Search
-Background:
-Given I am on FACT homepage
-And I navigate to the Search Page
-When I select "I do not have the name"
-Then I am presented with the "Find or contact a court - Find a Court or Tribunal - GOV.UK" page
-Then I select It is not listed here
-Then I am presented with the "Why do you need a court? - Find a Court or Tribunal - GOV.UK" page
+  Background:
+    Given I am on FACT homepage
+    And I navigate to the Search Page
+    When I select "I do not have the name"
+    Then I am presented with the "Find or contact a court - Find a Court or Tribunal - GOV.UK" page
+    Then I select It is not listed here
+    Then I am presented with the "Why do you need a court? - Find a Court or Tribunal - GOV.UK" page
 
-Scenario: Courts list display according to the alphabet selected
+  Scenario: Courts list display according to the alphabet selected
+    Given I selected I can not find what I am looking for
+    Then I am presented with the "Service not found - Find a Court or Tribunal - GOV.UK" page
+    When I Click on the link Search for a court by prefix (a-z)
+    Then I am presented with the "Search By Prefix - Find a Court or Tribunal - GOV.UK" page
+    When I clicked on alphabet Y
+    Then I can see courts list all start with "Y"
 
-  Given I selected I can not find what I am looking for
-  Then I am presented with the "Service not found - Find a Court or Tribunal - GOV.UK" page
-  When I Click on the link Search for a court by prefix (a-z)
-  Then I am presented with the "Search By Prefix - Find a Court or Tribunal - GOV.UK" page
-  When I clicked on alphabet Y
-  Then I can see courts list all start with Y
-  And court names are in alphabetical order
-
-
-Scenario: Letter with no court found
-  Given I selected I can not find what I am looking for
-  Then I am presented with the "Service not found - Find a Court or Tribunal - GOV.UK" page
-  When I Click on the link Search for a court by prefix (a-z)
-  Then I click on the letter with no courts eg X
-  Then I am presented with message that no court found
-
-
+  Scenario: Letter with no court found
+    Given I selected I can not find what I am looking for
+    Then I am presented with the "Service not found - Find a Court or Tribunal - GOV.UK" page
+    When I Click on the link Search for a court by prefix (a-z)
+    Then I click on the letter with no courts eg X
+    Then I am presented with message that no court found
 
   Scenario: court page validation
     Given I selected I can not find what I am looking for
     Then I am presented with the "Service not found - Find a Court or Tribunal - GOV.UK" page
     When I Click on the link Search for a court by prefix (a-z)
     Then I am presented with the "Search By Prefix - Find a Court or Tribunal - GOV.UK" page
-    When I clicked on alphabet B and i click on first court in the list
-    Then I am presented with the page containing court details
-
-
-
-
-
-
-
-
-
-
-
-
-
+    When I clicked on alphabet B then I click on first court in the list
+    Then I am presented with the "Bankruptcy Court (High Court) - Find a Court or Tribunal - GOV.UK" page
--- src/test/functional/steps/A-Z-court-search.ts
+++ src/test/functional/steps/A-Z-court-search.ts
@@ -1,29 +1,26 @@
-import { Given, When, Then } from 'cucumber';
-import { expect } from 'chai';
+import {Given, When, Then} from 'cucumber';
+import {expect} from 'chai';
 
 import * as I from '../utlis/puppeteer.util';
 
 
 When('I select It is not listed here', async () => {
-  const element='#not-listed';
+  const element = '#not-listed';
   const elementExist = await I.checkElement('#not-listed');
   expect(elementExist).equal(true);
   await I.click(element);
   await I.click('button[class=\'govuk-button govuk-!-margin-top-2 govuk-!-margin-bottom-8 continue\']');
 });
 
-Given('I selected I can not find what I am looking for', async function ()
-{
-  const element='#not-listed';
+Given('I selected I can not find what I am looking for', async function () {
+  const element = '#not-listed';
   const elementExist = await I.checkElement('#not-listed');
   expect(elementExist).equal(true);
   await I.click(element);
   await I.click('button[class=\'govuk-button govuk-!-margin-top-2 govuk-!-margin-bottom-8 continue\']');
-
 });
 
-When('I Click on the link Search for a court by prefix \\(a-z)',async function ()
-{
+When('I Click on the link Search for a court by prefix \\(a-z)', async function () {
   const element = await I.getElement('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > p:nth-child(6) > a:nth-child(1)');
   const elementExist = await I.checkElementIsAnchor(element);
   expect(elementExist).equal(true);
@@ -35,14 +32,21 @@ When('I clicked on alphabet Y', async function () {
   const elementExist = await I.checkElementIsAnchor(element);
   expect(elementExist).equal(true);
   await I.click('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > a:nth-child(27)');
-
 });
 
-Then('I can see courts list all start with Y', async function () {
-  // How the data comes back. I.e the data we will check to determine if it's sorted or not
+Then('I can see courts list all start with {string}', async (alphabet: string) => {
   const courtHtmlElement: [string] = await I.getHtmlFromElements('#courtList > h2 > a');
   expect(courtHtmlElement.length > 0).equal(true);
-  courtHtmlElement.forEach(courtName => expect(courtName.startsWith('Y')).equal(true));
+  courtHtmlElement.forEach(courtName => expect(courtName.startsWith(alphabet)).equal(true));
+  const sortedCourtNames: [string] = courtHtmlElement.sort();
+  let isEqual = true;
+  for (let i = 0; i < courtHtmlElement.length; ++i) {
+    if (courtHtmlElement[i] === sortedCourtNames[i])
+      isEqual = true;
+    else
+      isEqual = false;
+  }
+  expect(isEqual).equal(true);
 });
 
 Then('I click on the letter with no courts eg X', async function () {
@@ -50,14 +54,19 @@ Then('I click on the letter with no courts eg X', async function () {
   const elementExist = await I.checkElementIsAnchor(element);
   expect(elementExist).equal(true);
   await I.click('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > a:nth-child(26)');
+  const courtHtmlElement: [string] = await I.getHtmlFromElements('#courtList > h2 > a');
+  expect(courtHtmlElement.length < 1).equal(true);
 });
 
 Then('I am presented with message that no court found', async function () {
-  console.log('test');
+  const elementExist = await I.checkElement('#header-hint');
+  expect(elementExist).equal(true);
 });
 
-
-
-
-
-
+When('I clicked on alphabet B then I click on first court in the list', async function () {
+  const element = await I.getElement('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > a:nth-child(4)');
+  const elementExist = await I.checkElementIsAnchor(element);
+  expect(elementExist).equal(true);
+  await I.click('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > a:nth-child(4)');
+  await I.click('body > div:nth-child(7) > main:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > h2:nth-child(1) > a:nth-child(1)');
+});
