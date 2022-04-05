f8cd6e23720f4270fcbcd648414e2b9e482efbe4
Majid-justice <Majid.Najib@justice.gov.uk>
2022-04-05 17:08:08 +0100

    removing fix for demo

--- src/main/index.js
+++ src/main/index.js
@@ -63,8 +63,7 @@ cookieManager.init({
       'optional': false,
       'cookies': [
         'i18next',
-        'fact-cookie-preferences',
-        '_oauth2_proxy'
+        'fact-cookie-preferences'
       ]
     },
     {
