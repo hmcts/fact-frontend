126afd64acdf2c03bb22969a3c175928c3348fde
Imran Ali <Imran.Ali@justice.gov.uk>
2022-11-08 12:35:32 +0000

    Revert "courtDetailsController"
    This reverts commit a521e05eeb95aad4a2f6343f28376ec60b8bdc45.

--- src/main/controllers/CourtDetailsController.ts
+++ src/main/controllers/CourtDetailsController.ts
@@ -29,6 +29,7 @@ export class CourtDetailsController {
       const courtDetails: CourtDetailsResult = await this.api.court(slug, req.lng);
 
       if(!isObjectEmpty(courtDetails)) {
+
         if (courtDetails.open) {
           const viewData: CourtDetailsData = cloneDeep(req.i18n.getDataByLanguage(req.lng)['court-details']) as CourtDetailsData;
           viewData.title = viewData.title.replace('{courtName}', courtDetails.name);
@@ -42,15 +43,31 @@ export class CourtDetailsController {
             sendDocumentsEmail: filterByDescription(courtDetails.emails, ['send documents', 'anfon dogfennau']),
           };
 
-          this.handleImageFile(courtDetails);
+          if (courtDetails['image_file']) {
+            courtDetails['image_file'] = config.get('services.image-store.url') + '/' + courtDetails['image_file'];
+          }
 
           viewData.seoMetadata = generatePlaceMetadata(courtDetails);
           viewData.seoMetadataDescription = (viewData.seoMetadataDescription as string).replace('{courtName}', courtDetails.name);
           viewData.results = {...courtDetails, enquiries};
 
-          this.handleRender(req, res, viewData, courtDetails);
-        }
-        else {
+          if (courtDetails['in_person']) {
+            return res.render('court-details/in-person-court', viewData);
+          } else {
+
+            courtDetails['service_centre'] ?
+              viewData.notInPersonP1 = courtDetails.service_centre.intro_paragraph.length > 0 ? (req.lng == 'en'
+                ? courtDetails.service_centre.intro_paragraph : courtDetails.service_centre.intro_paragraph_cy) :
+                viewData.notInPersonP1
+                  .replace('{catchmentArea}', decideCatchmentArea(this.regionalCentre, viewData.catchmentArea))
+                  .replace('{serviceArea}', formatAreasOfLaw(courtDetails['areas_of_law']))
+              : viewData.notInPersonP1 = viewData.notInPersonP1
+                .replace('{catchmentArea}', decideCatchmentArea(this.regionalCentre, viewData.catchmentArea))
+                .replace('{serviceArea}', formatAreasOfLaw(courtDetails['areas_of_law']));
+
+            return res.render('court-details/not-in-person-court', viewData);
+          }
+        } else {
           const viewData: any = cloneDeep(req.i18n.getDataByLanguage(req.lng)['closed-court']);
           viewData.title = viewData.title.replace('{courtName}', courtDetails.name);
           viewData.name = courtDetails.name;
@@ -60,37 +77,5 @@ export class CourtDetailsController {
       }
     }
     next();
-  }
-
-  private handleRender(req: FactRequest, res: Response, viewData: CourtDetailsData, courtDetails: CourtDetailsResult): void {
-    if (courtDetails['in_person']) {
-      return res.render('court-details/in-person-court', viewData);
-    }
-    else
-    {
-      courtDetails['service_centre'] ?
-        viewData.notInPersonP1 = courtDetails.service_centre.intro_paragraph.length > 0 ?
-          this.chooseLanguage(req, courtDetails) :
-          this.replaceCatchmentAndServiceArea(viewData, courtDetails) :
-        viewData.notInPersonP1 = this.replaceCatchmentAndServiceArea(viewData, courtDetails);
-
-      return res.render('court-details/not-in-person-court', viewData);
-    }
-  }
-
-  private handleImageFile(courtDetails: CourtDetailsResult): void {
-    if (courtDetails['image_file']) {
-      courtDetails['image_file'] = config.get('services.image-store.url') + '/' + courtDetails['image_file'];
-    }
-  }
-
-  private chooseLanguage(req: FactRequest, courtDetails: CourtDetailsResult): string {
-    return req.lng == 'en' ? courtDetails.service_centre.intro_paragraph : courtDetails.service_centre.intro_paragraph_cy;
-  }
-
-  private replaceCatchmentAndServiceArea(viewData: CourtDetailsData, courtDetails: CourtDetailsResult): string {
-    return viewData.notInPersonP1
-      .replace('{catchmentArea}', decideCatchmentArea(this.regionalCentre, viewData.catchmentArea))
-      .replace('{serviceArea}', formatAreasOfLaw(courtDetails['areas_of_law']));
   }
 }
