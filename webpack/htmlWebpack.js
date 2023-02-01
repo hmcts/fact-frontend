dd3f4332590aa74015c9acc042e82a9b8e942e72
majid.najib <majid.najib@justice.gov.uk>
2023-02-01 16:38:20 +0000

    updating css and js for prod

--- webpack/htmlWebpack.js
+++ webpack/htmlWebpack.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const cssPath = path.resolve(__dirname, '../src/main/views/webpack/css.njk');
-const jsPath= path.resolve(__dirname, '../src/main/views/webpack/js.njk');
+const cssPath = path.resolve(__dirname, '../src/main/views/webpack/css-template.njk');
+const jsPath= path.resolve(__dirname, '../src/main/views/webpack/js-template.njk');
 
 
 const cssWebPackPlugin = new HtmlWebpackPlugin({
