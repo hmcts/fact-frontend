b4db3048b92e08b2ce5581f4fa3579305f1a8275
majid.najib <majid.najib@justice.gov.uk>
2023-02-01 11:41:44 +0000

    revert

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,10 +6,7 @@ const logger = Logger.getLogger('server');
 
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3100;
-const env = process.env.NODE_ENV || 'development';
 
 app.listen(port, () => {
   logger.info(`Application started: http://localhost:${port}`);
 });
-
-app.locals.ENV = env;
