b5aa18172a254c001b6f2ccb8f02050207b8c09f
amstevenson <addstevenson@hotmail.com>
2023-02-14 14:35:09 +0000

    test initial commit

--- src/test/config.ts
+++ src/test/config.ts
@@ -2,4 +2,22 @@ export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3100',
   TestHeadlessBrowser: true,
   TestSlowMo: 180,
+  WaitForTimeout: 10000,
+  Gherkin: {
+    features: './features/**/*.feature',
+    steps: ['../functional/steps/common.ts'],
+  },
+  helpers: {}
+};
+
+config.helpers = {
+  Playwright: {
+    url: config.TEST_URL,
+    show: !config.TestHeadlessBrowser,
+    browser: 'chromium',
+    waitForTimeout: config.WaitForTimeout,
+    waitForAction: 1000,
+    waitForNavigation: 'networkidle0',
+    ignoreHTTPSErrors: true,
+  },
 };
