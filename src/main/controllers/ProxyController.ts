d50374a699170dfa45e6677aae97b164b936c188
majid.najib <majid.najib@justice.gov.uk>
2023-03-21 18:02:13 +0000

    add error page

--- src/main/controllers/ProxyController.ts
+++ src/main/controllers/ProxyController.ts
@@ -2,6 +2,7 @@ import { FactRequest } from '../interfaces/FactRequest';
 import { Response } from 'express';
 import { FactApi } from '../utils/FactApi';
 import autobind from 'autobind-decorator';
+import {hasErrors} from '../utils/validation';
 
 @autobind
 export class ProxyController {
@@ -12,17 +13,18 @@ export class ProxyController {
 
   public async getCourtDetails(req: FactRequest, res: Response): Promise<void> {
     const court = await this.api.court(req.params.slug, req.lng);
-    res.send(court);
+    hasErrors(court) ? res.render('error', req.i18n.getDataByLanguage(req.lng).error) : res.send(court);
   }
 
 
   public async getCourtsByPostcodeServiceArea(req: FactRequest, res: Response): Promise<void> {
     const results = await this.api.postcodeServiceAreaSearch(req.params.postcode, req.params.serviceArea, req.params.action, req.lng);
-    res.send(results);
+    hasErrors(results) ? res.render('error', req.i18n.getDataByLanguage(req.lng).error) : res.send(results);
   }
 
   public async getCourtsByCourtTypes(req: FactRequest, res: Response): Promise<void> {
     const results = await this.api.courtTypesSearch(req.params.courtTypes);
-    res.send(results);
+    hasErrors(results) ? res.render('error', req.i18n.getDataByLanguage(req.lng).error) : res.send(results);
   }
+
 }
