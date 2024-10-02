71c705ead7ffbdd5a215b2849e75931d90c2f8e3
Jay Ashton <jay.ashton@justice.gov.uk>
2024-10-02 15:24:49 +0100

    Add type checks and linting

--- playwright-e2e/test/signIn.spec.ts
+++ playwright-e2e/test/signIn.spec.ts
@@ -1,8 +1,9 @@
-import { test, expect } from '@playwright/test';
-import SignIn from '../pages/signIn.page'; 
+import { test, expect } from "@playwright/test";
+import { SignIn } from "../pages/signIn.page";
 
-test('Check if the sign-in heading is visible', async ({ page }) => {
+test("Check if the sign-in heading is visible", async ({ page }) => {
   // Initialize the SignIn page object and check is heading is visible
   const signInPage = new SignIn(page);
+  await signInPage.goto();
   await expect(signInPage.heading).toBeVisible();
 });
