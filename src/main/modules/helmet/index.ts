8d86c77fe6ac5c13c09acee278dde1785cb0e359
amstevenson <addstevenson@hotmail.com>
2024-11-21 14:20:45 +0000

    helmet changes

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -18,11 +18,62 @@ export class Helmet {
   constructor(public config: HelmetConfig) {}
 
   public enableFor(app: express.Express): void {
-    // include default helmet functions
-    app.use(helmet({crossOriginEmbedderPolicy: false}));
+    // include default helmet functions, disabling specific ones to customize
+    app.use(helmet());
 
+    // Manually set required headers
+    this.setStrictTransportSecurity(app);
+    this.setXFrameOptions(app);
+    this.setPermissionsPolicy(app);
+    this.setCustomHeaders(app);
     this.setContentSecurityPolicy(app);
     this.setReferrerPolicy(app, this.config.referrerPolicy);
+  }
+
+  private setStrictTransportSecurity(app: express.Express): void {
+    app.use(
+      helmet.hsts({
+        maxAge: 31536000, // 1 year in seconds
+        includeSubDomains: true,
+        preload: true,
+      }),
+    );
+  }
+
+  private setXFrameOptions(app: express.Express): void {
+    app.use(helmet.frameguard({ action: 'deny' }));
+  }
+
+  private setPermissionsPolicy(app: express.Express): void {
+    app.use(
+      helmet.permittedCrossDomainPolicies({
+        permittedPolicies: 'none',
+      }),
+    );
+    app.use((_req, res, next) => {
+      res.setHeader(
+        'Permissions-Policy',
+        'geolocation=(), camera=(), microphone=(), interest-cohort=()',
+      );
+      next();
+    });
+  }
+
+  private setCustomHeaders(app: express.Express): void {
+    // Custom headers not natively supported by Helmet
+    app.use((_req, res, next) => {
+      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+      // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+      res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://www.find-court-tribunal.service.gov.uk',
+      );
+      // Removed as per OWASP recommendations
+      res.removeHeader('Expect-CT');
+
+      next();
+    });
   }
 
   private setContentSecurityPolicy(app: express.Express): void {
@@ -31,10 +82,26 @@ export class Helmet {
         directives: {
           connectSrc: [self, googleAnalyticsDomain, doubleclick, 'https://*.dynatrace.com'],
           defaultSrc: ["'none'"],
-          fontSrc: [self, 'data:'],
-          imgSrc: [self, azureBlob, ...tagManager, googleAnalyticsDomain, 'data:', 'https://*.dynatrace.com'],
+          fontSrc: [self, 'data:', 'https://fonts.gstatic.com'],
+          imgSrc: [
+            self,
+            azureBlob,
+            ...tagManager,
+            googleAnalyticsDomain,
+            'data:',
+            'https://ssl.gstatic.com',
+            'https://www.gstatic.com',
+            'https://*.dynatrace.com',
+          ],
           objectSrc: [self],
-          scriptSrc: [self, ...tagManager, googleAnalyticsDomain, "'unsafe-inline'", "'unsafe-eval'", 'https://*.dynatrace.com'],
+          scriptSrc: [
+            self,
+            ...tagManager,
+            googleAnalyticsDomain,
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            'https://*.dynatrace.com',
+          ],
           styleSrc: [self, ...tagManager, "'unsafe-inline'", 'https://fonts.googleapis.com'],
         },
       }),
