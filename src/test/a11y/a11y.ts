a6564f5177ee666048ba94bba30d6136e4ec089b
Imran Ali <Imran.Ali@justice.gov.uk>
2025-05-08 11:34:31 +0100

    Revert "more updates"
    This reverts commit ecf2089936a821c0cb181968f77b113b78ccca5f.

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -70,8 +70,6 @@ function ensurePageCallWillSucceed(url: string): Promise<void> {
 function runPallyWith(url: string, actions: string[]): Pa11yResult {
   return pa11y(config.TEST_URL + url, {
     hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
-    // timeout: 30000,
-    // wait: 1000,
     actions: actions
   });
 }
@@ -86,7 +84,6 @@ function expectNoErrors(messages: PallyIssue[]): void {
 
 function testAccessibilityWithActions(url: string, actions: string[]): void {
   describe(`Page ${url}`, () => {
-    // jest.setTimeout(60000);
     test('should have no accessibility errors', done => {
       ensurePageCallWillSucceed(url)
         .then(() => runPallyWith(url, actions))
