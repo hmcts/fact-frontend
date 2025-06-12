4470d1bda5ad218cf1f1282df51a90bbd7f00417
abigailflorence97 <abigail.smith@hmcts.net>
2025-06-12 10:29:25 +0100

    rework idam test following changes in library

--- playwright-e2e/tests/create-user-test.spec.ts
+++ playwright-e2e/tests/create-user-test.spec.ts
@@ -2,21 +2,28 @@ import { test } from "../fixtures";
 import { v4 as uuidv4 } from "uuid";
 
 test("Create citizen user", async ({ idamUtils }) => {
-    const token = process.env.CREATE_USER_BEARER_TOKEN as string;
-    const uniqueId = uuidv4();
-    const email = `TEST_PRL_USER_citizen.${uniqueId}@test.local`;
-    const forename = "fn_" + uniqueId.split("-")[0];
-    const surname = "sn_" + uniqueId.split("-")[1];
-    const password = process.env.CITIZEN_PASSWORD as string;
+  // Bearer token was generated in global.setup.ts and is pulled via environment variable.
+  const token = process.env.CREATE_USER_BEARER_TOKEN as string; 
+  const uniqueId = uuidv4();
 
-    const user = await idamUtils.createUser({
-        bearerToken: token,
-        email,
-        password,
-        forename,
-        surname,
-        roleNames: ["citizen"]
-    });
+  // You can change these user details (email, forename, surname, etc.) to suit your needs.
+  // The uniqueId ensures each user is distinct, minimising overlap when creating multiple test users.
+  const email = `TEST_PRL_USER_citizen.${uniqueId}@test.local`;
+  const forename = "fn_" + uniqueId.split("-")[0];
+  const surname = "sn_" + uniqueId.split("-")[1];
+  const password = process.env.CITIZEN_PASSWORD as string; // Password is securely pulled from Azure Key Vault via environment variable.
+  
+  //Create a citizen user using IdamUtils fixture in playwright common library
+  const user = await idamUtils.createUser({
+    bearerToken: token,
+    password,
+    user: {
+      email,
+      forename,
+      surname,
+      roleNames: ["citizen"], 
+    },
+  });
 
-    console.log(user);
+  console.log(user);
 });
