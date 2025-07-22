9d36b3da338182da0167ef029a39649626e04ed1
Dan-Marius Bradea <bradeadanmarius@gmail.com>
2025-07-22 11:14:39 +0100

    fix: resolved missing module errors in VS Code by generating PnP SDKs for Playwright

--- src/test/playwright-e2e/page-objects/components/cookies.header.ts
+++ src/test/playwright-e2e/page-objects/components/cookies.header.ts
@@ -1,19 +1,15 @@
 import { Locator, Page } from '@playwright/test';
-import { expect } from '../../fixtures.ts';
+import { expect } from '../../fixtures';
 
 export class cookiesHeader {
-  readonly acceptCookiesButton = this.root.locator(
-    'button.cookie-banner-accept-button',
-    { hasText: 'Accept additional cookies' },
-  );
+  readonly acceptCookiesButton = this.root.locator('button.cookie-banner-accept-button', {
+    hasText: 'Accept additional cookies',
+  });
   readonly hideMessageButton = this.root.getByRole('button', {
     name: 'Hide this message',
   });
 
-  constructor(
-    private root: Locator,
-    private page: Page,
-  ) {}
+  constructor(private root: Locator, private page: Page) {}
 
   async acceptCookies() {
     if (await this.acceptCookiesButton.isVisible()) {
