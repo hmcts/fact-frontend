73d33f867689acdc0cb3a1e7b2fe8f495556fc82
Imran Ali <101260174+justiceia@users.noreply.github.com>
2025-09-15 07:49:01 +0100

    FACT-2066 point app insights to new instance DO NOT MERGE (#880)
    * FACT-2066 point app insights to new instance
    
    * Bumping chart version/ fixing aliases
    
    * added into aat yaml
    
    * Revert "added into aat yaml"
    
    This reverts commit f2370a2fea351593f9b832cd56309e1a16ae9cc6.
    
    * added name aliases instead
    
    * undo aliasing
    
    * axios 1.11 to 1.12
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,9 +9,9 @@ export class PropertiesVolume {
     if (!app.locals.developmentMode) {
       propertiesVolume.addTo(config);
 
-      this.setSecret('secrets.fact.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+      this.setSecret('secrets.fact.AppInsightsInstrumentationKey-ai', 'appInsights.instrumentationKey');
     } else {
-      this.setLocalSecret('AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+      this.setLocalSecret('AppInsightsInstrumentationKey-ai', 'appInsights.instrumentationKey');
     }
   }
 
