48c0fa7407a39eb7de099d8134946cf01e50d994
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-10-28 17:12:58 +0000

    Override ID

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,6 +6,7 @@ const logger = Logger.getLogger('server');
 
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3100;
+process.env.AZURE_CLIENT_ID = process.env.POC_CLIENT_APP_REG_ID;
 
 app.listen(port, () => {
   logger.info(`Application started: http://localhost:${port}`);
