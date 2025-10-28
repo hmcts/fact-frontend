2708b49688ba8b1959bd714897b2cc4f9f7ce331
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-10-28 17:33:39 +0000

    comment and update secret set

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const { Logger } = require('@hmcts/nodejs-logging');
 import { app } from './app';
+import config from 'config';
 
 const logger = Logger.getLogger('server');
 
 // TODO: set the right port for your application
 const port: number = parseInt(process.env.PORT, 10) || 3100;
-process.env.AZURE_CLIENT_ID = process.env.POC_CLIENT_APP_REG_ID;
+process.env.AZURE_CLIENT_ID = config.get("poc.clientAppRegId");
 
 app.listen(port, () => {
   logger.info(`Application started: http://localhost:${port}`);
